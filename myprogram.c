897a7f58d9f92872e467bc14b44c65209cf8c931
root <root@DESKTOP-8T5TT0C.localdomain>
2020-10-11 16:09:47 +0300

    method 5 is done

--- myprogram.c
+++ myprogram.c
@@ -4,7 +4,8 @@ int sum_of_1_to_n(int n)
 	int sum = 0;
 
 	//TODO: sum all numbers from 1 to n
-	for(int i = 1; i <= n; i++){
+	for(int i = 1; i <= n; i++)
+	{
 		sum += i;
 	}
 	return sum;
@@ -16,7 +17,8 @@ int sum_of_even_numbers(int *array, int count)
 	for (int i=0;i<count;++i)
 	{
 		//TODO: only add even numbers, e.g., 4. Skip odd numbers, e.g., 3
-		if (array[i] % 2 == 0){
+		if (array[i] % 2 == 0)
+		{
 			sum += array[i];
 		}
 	}
@@ -26,8 +28,10 @@ int sum_of_even_numbers(int *array, int count)
 int max_of_numbers(int *array, int count)
 {
 	//TODO: return the maximum number from the array
-		for (int i = 1; i < count; ++i){
-			if (array[0] < array[i]){
+		for (int i = 1; i < count; ++i)
+		{
+			if (array[0] < array[i])
+			{
 				array[0] = array[i];
 			}
 		}
@@ -40,7 +44,8 @@ int reversed_number(int number)
 
 	//TODO: if input is 12345, return 54321
 	reversed = 0;
-	while(number != 0){
+	while(number != 0)
+	{
 		reversed = reversed * 10 + number % 10;
 		number /= 10;
 	}
@@ -50,8 +55,16 @@ int reversed_number(int number)
 int is_prime(int number)
 {
 	//TODO: return 1 if the input number is prime, otherwise 0
+	//sqrt(number) would be more efficient but since we do not
+	//use math lib, I simple used number/2 instead
 
-	return 0;
+	for(int i = 2; i < number /2; i++)
+	{ 
+		if (number % i == 0){
+			return 0;
+		}
+	}
+	return 1;
 }
 
 int count_primes(int start, int end)
